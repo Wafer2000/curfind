d08fe99957596b3ec66ddcdd6c48223bac25099c
Mario Fernando Gonsalvez Arias <mariofernandogonsalvez@gmai.com>
2024-02-05 21:24:28 -0500

    Creacion de Firebase Auth

---
+++ lib/service/firebase_auth.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FirebaseAuth extends StatefulWidget {
+  const FirebaseAuth({super.key});
+
+  @override
+  State<FirebaseAuth> createState() => _FirebaseAuthState();
+}
+
+class _FirebaseAuthState extends State<FirebaseAuth> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
