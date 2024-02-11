d592867f20e7d32e32b1e9bee8e2af9775699d4d
Mario Fernando Gonsalvez Arias <mariofernandogonsalvez@gmai.com>
2024-02-10 22:26:48 -0500

    Completado completamente el cambio de estados por color en el menu bajo

--- lib/components/routes/screens.dart
+++ lib/components/routes/screens.dart
@@ -4,6 +4,7 @@ import 'package:curfind/components/routes/views/history.dart';
 import 'package:curfind/components/routes/views/home.dart';
 import 'package:curfind/components/routes/views/menssages.dart';
 import 'package:curfind/components/routes/views/perfil.dart';
+import 'package:curfind/style/global_colors.dart';
 import 'package:flutter/material.dart';
 
 class Screens extends StatefulWidget {
@@ -17,37 +18,11 @@ class _ScreensState extends State<Screens> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   bool? _isSwitched;
   int selectedIndex = 0;
-  Color iconColor = Colors.purple;
-
-  @override
-  void initState() {
-    super.initState();
-    _getData();
-  }
-
-  Future<void> _getData() async {
-    try {
-      final snapshot = await _firestore
-          .collection('ColorEstado')
-          .doc('7HQdmSTNdcE8hYmFYYmf')
-          .snapshots()
-          .first;
-
-      if (snapshot.exists) {
-        setState(() {
-          _isSwitched = snapshot.data()!['Estado'];
-          _isSwitched == false
-              ? iconColor = Colors.green
-              : iconColor = Colors.purple;
-        });
-      }
-    } catch (e) {
-      print('Error al obtener el dato: $e');
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
+    Color iconColor = _isSwitched == true ? IconColor.purple().color : IconColor.green().color;
+
     final screens = [
       const Messages(),
       const History(),
@@ -56,57 +31,71 @@ class _ScreensState extends State<Screens> {
       const Perfil()
     ];
 
-    return Scaffold(
-      body: IndexedStack(
-        index: selectedIndex,
-        children: screens,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.shifting,
-        currentIndex: selectedIndex,
-        onTap: (value) {
-          setState(() {
-            selectedIndex = value;
-          });
-        },
-        elevation: 0,
-        items: [
-          BottomNavigationBarItem(
-              icon: Image.asset('assets/message.png',
-                  width: 29.9, height: 29.9, color: iconColor),
-              label: ''),
-          BottomNavigationBarItem(
-              icon: Image.asset(
-                'assets/history.png',
-                width: 29.6,
-                height: 26.3,
-                color: iconColor,
-              ),
-              label: ''),
-          BottomNavigationBarItem(
-              icon: Image.asset(
-                'assets/logo_curfind.png',
-                width: 28.2,
-                height: 44.6,
-              ),
-              label: ''),
-          BottomNavigationBarItem(
-              icon: Image.asset(
-                'assets/companie.png',
-                width: 29.6,
-                height: 29.6,
-                color: iconColor,
-              ),
-              label: ''),
-          const BottomNavigationBarItem(
-              icon: CircleAvatar(
-                backgroundImage: AssetImage(
-                  'assets/photo_perfil.png',
-                ),
-              ),
-              label: '')
-        ],
-      ),
-    );
+    return StreamBuilder<DocumentSnapshot>(
+        stream: _firestore
+            .collection('ColorEstado')
+            .doc('7HQdmSTNdcE8hYmFYYmf')
+            .snapshots(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+
+          _isSwitched = snapshot.data?['Estado'];
+          iconColor = _isSwitched == true ? IconColor.purple().color : IconColor.green().color;
+
+          return Scaffold(
+            body: IndexedStack(
+              index: selectedIndex,
+              children: screens,
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.shifting,
+              currentIndex: selectedIndex,
+              onTap: (value) {
+                setState(() {
+                  selectedIndex = value;
+                });
+              },
+              elevation: 0,
+              items: [
+                BottomNavigationBarItem(
+                    icon: Image.asset('assets/message.png',
+                        width: 29.9, height: 29.9, color: iconColor),
+                    label: ''),
+                BottomNavigationBarItem(
+                    icon: Image.asset(
+                      'assets/history.png',
+                      width: 29.6,
+                      height: 26.3,
+                      color: iconColor,
+                    ),
+                    label: ''),
+                BottomNavigationBarItem(
+                    icon: Image.asset(
+                      'assets/logo_curfind.png',
+                      width: 28.2,
+                      height: 44.6,
+                    ),
+                    label: ''),
+                BottomNavigationBarItem(
+                    icon: Image.asset(
+                      'assets/companie.png',
+                      width: 29.6,
+                      height: 29.6,
+                      color: iconColor,
+                    ),
+                    label: ''),
+                const BottomNavigationBarItem(
+                    icon: CircleAvatar(
+                      backgroundImage: AssetImage(
+                        'assets/photo_perfil.png',
+                      ),
+                    ),
+                    label: '')
+              ],
+            ),
+          );
+        });
+
+    /**/
   }
 }
--- lib/components/routes/views/home.dart
+++ lib/components/routes/views/home.dart
@@ -10,28 +10,34 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  bool _isUpdating = false;
 
   Future<void> _updateText() async {
-    try {
-      DocumentSnapshot<Map<String, dynamic>> snapshot = await _firestore
-          .collection('ColorEstado')
-          .doc('7HQdmSTNdcE8hYmFYYmf')
-          .get();
-
-      if (snapshot.exists) {
-        // Toggle the value of the 'Estado' field to its opposite boolean value
-        bool newValue = !snapshot.data()!['Estado'];
-
-        // Call the 'update' method to save the updated value to Firebase Firestore
-        await _firestore
+    if (!_isUpdating) {
+      _isUpdating = true;
+      try {
+        DocumentSnapshot<Map<String, dynamic>> snapshot = await _firestore
             .collection('ColorEstado')
             .doc('7HQdmSTNdcE8hYmFYYmf')
-            .update({
-          'Estado': newValue,
-        });
+            .get();
+
+        if (snapshot.exists) {
+          // Toggle the value of the 'Estado' field to its opposite boolean value
+          bool newValue = !snapshot.data()!['Estado'];
+
+          // Call the 'update' method to save the updated value to Firebase Firestore
+          await _firestore
+              .collection('ColorEstado')
+              .doc('7HQdmSTNdcE8hYmFYYmf')
+              .update({
+            'Estado': newValue,
+          });
+        }
+      } catch (e) {
+        print('Error al actualizar el texto: $e');
+      } finally {
+        _isUpdating = false;
       }
-    } catch (e) {
-      print('Error al actualizar el texto: $e');
     }
   }
 
@@ -39,11 +45,15 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return GestureDetector(
         onPanUpdate: (DragUpdateDetails details) {
-          if (details.delta.dx > 5) {
-            _updateText();
+          if (details.delta.dx > 5000000000000000000 && !_isUpdating) {
+            Future.delayed(Duration(milliseconds: 100), () {
+              _updateText();
+            });
           }
-          if (details.delta.dx < 5) {
-            _updateText();
+          if (details.delta.dx < 5000000000000000000 && !_isUpdating) {
+            Future.delayed(Duration(milliseconds: 100), () {
+              _updateText();
+            });
           }
         },
         child: const Scaffold(
